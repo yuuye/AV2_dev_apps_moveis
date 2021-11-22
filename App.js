a9a79c995db769aee4d9f4586efda9ea3b8fbfe8
yuuye <82718965+yuuye@users.noreply.github.com>
2021-11-22 00:01:47 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import { LogBox } from 'react-native';
+import { UsuarioProvider } from './contexts/user';
+
+export default function App() {
+
+  LogBox.ignoreLogs(['Setting a timer', 'AsyncStorage has been!', 'Bottom Tab'])
+  return (
+    <UsuarioProvider>
+      <Routes />
+    </UsuarioProvider>
+  );
+}
+
+
